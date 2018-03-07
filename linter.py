346c9196cceffeb5ea2ffd573b43dc6b2853dae0
Bastiaan Veelo <Bastiaan@SARC.nl>
2018-03-07 01:48:23 +0100

    SublimeLinter 4 has changed the temp file/path.
    It's actually less obvious to recognise the relevant rows of output, let's hope there are not too many false positives.

--- linter.py
+++ linter.py
@@ -1,9 +1,9 @@
 #
 # linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+# Linter for SublimeLinter4, a code checking framework for Sublime Text 3
 #
 # Written by Bastiaan N. Veelo
-# Copyright (c) 2017 Bastiaan N. Veelo
+# Copyright (c) 2017, 218 Bastiaan N. Veelo
 #
 # License: MIT
 #
@@ -28,13 +28,13 @@ class Dmd(Linter):
     version_requirement = '>= 2.076.1'
     regex = (
         # Temporary source file (ignored) followed by "(row,col): "
-        r'^.+SublimeLinter3.+[(](?P<line>\d+)([,](?P<col>\d+))?[)]: '
+        r'^.+tmp.+[(](?P<line>\d+)([,](?P<col>\d+))?[)]: '
         # Either "Error: ", "Warning: " or "Deprecation: "
         r'(?:(?P<error>Error)|(?P<warning>Warning|Deprecation)): '
         # Single-row message
         r'(?P<message>.+)\r?\n'
         # Ignored rows (without the path to the temp source file)
-        r'((.(?!SublimeLinter3))+\r?\n)*'
+        r'((.(?!tmp))+\r?\n)*'
     )
     multiline = True
     tempfile_suffix = 'd'
